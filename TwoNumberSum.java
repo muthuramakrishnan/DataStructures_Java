41e6edb1c31edb4bd9d2e6031e673f8f1fcfcd1e
Muthuramakrishnan M <muthu.rama.krishnan96@gmail.com>
2022-03-14 07:22:35 +0530

    TwoNumberSum - hashing approach

--- TwoNumberSum.java
+++ TwoNumberSum.java
@@ -1,8 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Scanner;
 
 public class TwoNumberSum {
 
+    public static int[] hashTableApproach(int[] array, int target){
+        //O(n)
+        Set s = new HashSet();
+
+        for(int i=0; i<array.length; i++){
+            int num = target - array[i];
+            if(s.contains(num)){
+                return new int[] {num, array[i]};
+            }
+            else {
+                s.add(array[i]);
+            }
+        }
+        return new int[] {};
+    }
     public static int[] bruteForceTwoNumberSum(int[] array, int target){
+//        o(n2)
         for(int i=0; i<array.length; i++){
             for(int j=i+1; j<array.length; j++){
                 if(array[i] + array[j] == target){
@@ -25,12 +43,13 @@ public class TwoNumberSum {
         }
 
         int[] result;
-        result = bruteForceTwoNumberSum(array, target);
+//        result = bruteForceTwoNumberSum(array, target);
+        result = hashTableApproach(array, target);
         if(result.length == 0){
             System.out.println("No such numbers");
         }
         for(int item : result){
-            System.out.println(item+" ");
+            System.out.print(item+" ");
         }
     }
 }
