2f9b00871c08950391b46d620f348cb98122e1f0
Muthuramakrishnan M <muthu.rama.krishnan96@gmail.com>
2022-03-16 06:32:34 +0530

    Rotate array by reversals

--- RotateArray.java
+++ RotateArray.java
@@ -1,6 +1,31 @@
 import java.util.Scanner;
 
 public class RotateArray {
+    public static void reverseArray(int[] array, int start, int end){
+        while (start < end)
+        {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void rotateArrayByReversal(int[] array, int k){
+        reverseArray(array, 0, array.length-1);
+        reverseArray(array, 0, k-1);
+        reverseArray(array, k, array.length-1);
+    }
+    public static int[] rotateArrayOptimized(int[] array, int k){
+        //this will have o(n) time complexity
+        //o(n) space complexity
+        int n = array.length;
+        int[] result = new int[n];
+        for(int i=0; i<n; i++){
+            result[i] = array[(i+k)%n];
+        }
+        return result;
+    }
     public static void rotateArrayOneUnit(int[] array){
         int temp = array[array.length-1];
         for(int i=array.length-2; i>=0; i--){
@@ -20,8 +45,10 @@ public class RotateArray {
             array[i] = s.nextInt();
         }
         //brute force way of rotating n units
-        for(int i=0; i<k; i++)
-        rotateArrayOneUnit(array);
+//        for(int i=0; i<k; i++)
+//        rotateArrayOneUnit(array);
+//        int[] rotatedArray = rotateArrayOptimized(array, k);
+        rotateArrayByReversal(array, k);
         for(int item : array){
             System.out.print(item + " ");
         }
