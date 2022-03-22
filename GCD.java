c858ce4d9cce2db2f63fe0622a40f05860e11244
Muthuramakrishnan M <muthu.rama.krishnan96@gmail.com>
2022-03-22 23:41:46 +0530

    gcd by repeated division

--- GCD.java
+++ GCD.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 
 public class GCD {
+    public static void gcdByRepeatedDivision(int n1, int n2){
+        int n = Math.min(n1, n2);
+        int dividend = Math.max(n1, n2);
+        while(n!=1){
+            int r = dividend % n;
+            if(r==0){
+                System.out.println(n);
+                return;
+            }
+            else{
+                dividend = n;
+                n = r;
+            }
+        }
+        System.out.println(1);
+    }
     public static void findGCDBruteForce(int n1, int n2){
         int maxNo = Math.max(n1,n2);
         int gcd = 1;
@@ -20,5 +36,6 @@ public class GCD {
         n2 = s.nextInt();
 
         findGCDBruteForce(n1, n2);
+        gcdByRepeatedDivision(n1, n2);
     }
 }
