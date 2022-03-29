4fb1c1ceb2cfb71ea5a735218a814bf77466440b
Muthuramakrishnan M <muthu.rama.krishnan96@gmail.com>
2022-03-29 15:07:07 +0530

    lcm optimized

--- LCM.java
+++ LCM.java
@@ -1,6 +1,25 @@
 import java.util.Scanner;
 
 public class LCM {
+    public static int gcdByRepeatedDivision(int n1, int n2){
+        int n = Math.min(n1, n2);
+        int dividend = Math.max(n1, n2);
+        while(n!=1){
+            int r = dividend % n;
+            if(r==0){
+                return n;
+            }
+            else{
+                dividend = n;
+                n = r;
+            }
+        }
+        return 1;
+    }
+    public static int findLcmOptimized(int n1, int n2){
+        int gcd = gcdByRepeatedDivision(n1, n2);
+        return (n1*n2) / gcd;
+    }
     public static int findLcm(int n1, int n2){
         //lcm lower bound is max(n1, n2)
         //lcm upper bound is n1*n2
@@ -20,7 +39,8 @@ public class LCM {
         n1 = s.nextInt();
         n2 = s.nextInt();
 
-        int lcm = findLcm(n1, n2);
+//        int lcm = findLcm(n1, n2);
+        int lcm = findLcmOptimized(n1, n2);
         System.out.println(lcm);
 
     }
