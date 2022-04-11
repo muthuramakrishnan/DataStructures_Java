896a5a3791cf2b4cb3531f9495eb02192f55b26c
Muthuramakrishnan M <muthu.rama.krishnan96@gmail.com>
2022-04-11 21:14:35 +0530

    prime factorization

---
+++ PrimeFactorization.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PrimeFactorization {
+    public static boolean isPrime(int n){
+        if(n<2) return false;
+        for(int i=2; i*i<n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void bruteForceApproach(int n){
+        List<Integer> factors = new ArrayList<>();
+        for(int i=2; i<=n; i++){
+            if(isPrime(i)){
+                while(n%i==0){
+                    factors.add(i);
+                    n = n/i;
+                }
+            }
+        }
+        System.out.println(factors);
+    }
+    public static void main(String[] args){
+        int n;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        //brute force approach
+        bruteForceApproach(n);
+    }
+}
